24ba7b97ed6d048c45868e5e82463bb650dddac7
Gabewest1 <gabe_west1@yahoo.com>
2017-08-25 04:03:25 -0500

    Added functions and event handlers to handle updating the video time progress bar and the volume bar.

--- VideoControls.js
+++ VideoControls.js
@@ -1,9 +1,31 @@
 class VideoControls {
     constructor(video) {
-        this.video = document.querySelector("iframe")
-        this.video.controls = false
-        console.log(this.video)
-        this.video.addEventListener("play", this.pauseVideo)
+        this.video = document.querySelector(".video-container video")
+        this.videoProgressBar = document.querySelector(".video-progress-bar")
+        this.playButton = document.querySelector(".play-btn")
+        this.skipForwardsButton = document.querySelector(".skip-forward-btn")
+        this.skipBackwardsButton = document.querySelector(".skip-back-btn")
+        this.volumeBar = document.querySelector(".volume-bar")
+
+        /* Setup event listeners */
+        this.video.addEventListener("timeupdate", () => this.setVideoProgressBar(this.video.currentTime))
+        this.video.addEventListener("canplay", () => this.videoProgressBar.max = Math.floor(this.video.duration))
+        this.videoProgressBar.addEventListener("change", () => this.video.currentTime = this.videoProgressBar.value)
+        this.playButton.addEventListener("click", this.toggleVideo.bind(this))
+        this.skipForwardsButton.addEventListener("click", this.skipForwards.bind(this))
+        this.skipBackwardsButton.addEventListener("click", this.skipBackwards.bind(this))
+        this.volumeBar.addEventListener("change", (e) => this.setVolume(e.target.value))
+        
+        //Set the volume to match the default value of the volumeBar
+        this.setVolume(this.volumeBar.value)
+    }
+
+    setVideoProgressBar(value) {
+        this.videoProgressBar.value = value
+    }
+
+    toggleVideo() {
+        this.video.paused ? this.playVideo() : this.pauseVideo()
     }
 
     playVideo() {
@@ -29,6 +51,11 @@ class VideoControls {
 
     skipForwards() {
         this.video.currentTime = Math.min(this.video.currentTime + 7, this.video.duration)
+    }
+
+    setVolume(value) {
+        console.log("SETTING VOLUME:", value)
+        this.video.volume = value
     }
 }
 
